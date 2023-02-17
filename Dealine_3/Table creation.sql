fa8b6da0f2a64afeb2cf35a04222bf92c3d2a36f
Prakhar-Gupta-03 <114860927+Prakhar-Gupta-03@users.noreply.github.com>
2023-02-17 23:54:11 +0530

    new table structure

--- Dealine_3/Table creation.sql
+++ Dealine_3/Table creation.sql
@@ -1,55 +1,93 @@
-use test;
-create table Admin(
-    Admin_ID INT NOT NULL AUTO_INCREMENT,
-    Admin_Password VARCHAR(20) NOT NULL,
-    PRIMARY KEY (Admin_ID)
-)
-CREATE TABLE Product (
-	Product_Name VARCHAR(50),
-	Product_Quantity VARCHAR(50),
-	Category_ID INT NOT NULL,
-	Product_Price VARCHAR(50),
-	Product_Offer VARCHAR(50),
-	Product_ID INT NOT NULL,
-    PRIMARY KEY (Category_ID)
-    -- PRIMARY KEY (Category_ID)
+USE test;
+
+DROP TABLE IF EXISTS DELIVERY_MAN;
+DROP TABLE IF EXISTS CART;
+DROP TABLE IF EXISTS ORDERS;
+DROP TABLE IF EXISTS PRODUCT;
+DROP TABLE IF EXISTS CUSTOMER;
+DROP TABLE IF EXISTS CATEGORY;
+DROP TABLE IF EXISTS ADMIN_SHOP;
+
+CREATE Table admin_shop(
+    Admin_ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    ADMIN_NAME VARCHAR(50) NOT NULL,
+    Admin_Password VARCHAR(50) NOT NULL
 );
-CREATE TABLE category(
-    Category_ID INT NOT NULL AUTO_INCREMENT,
-    Sup_cat_ID INT,
-    Category_Name VARCHAR(50) NOT NULL,
-    PRIMARY KEY (Category_ID),
-    FOREIGN KEY (Sup_cat_ID) REFERENCES Product(Category_ID)
-);
-create table cart(
-    Cart_ID INT NOT NULL AUTO_INCREMENT,
-    Cart_Total_Price INT NOT NULL, 
-    Cart_Product_ID INT NOT NULL,
-    Cart_Product_Quantity INT NOT NULL,
-    PRIMARY KEY (Cart_ID),
-    -- FOREIGN KEY (Cart_Product_ID) REFERENCES product(Product_ID)
-);
-create table customer(
-    Customer_ID INT NOT NULL AUTO_INCREMENT, 
-    Customer_First_Name VARCHAR(50) NOT NULL,
-    Customer_Last_Name VARCHAR(50) NOT NULL,
-    Customer_Address_BuildingNo VARCHAR(15) NOT NULL,
+
+CREATE Table customer(
+    customer_fname VARCHAR(50) NOT NULL,
+    customer_lname VARCHAR(50) NOT NULL,
+    customer_email VARCHAR(50) NOT NULL,
+    customer_password VARCHAR(50) NOT NULL,
+    customer_wallet INT Default 0,
+    Customer_Address_BuildingNo int NOT NULL,
     Customer_Address_Street VARCHAR(50) NOT NULL,
     Customer_Address_City VARCHAR(50) NOT NULL,
     Customer_Address_State VARCHAR(50) NOT NULL,
-    Customer_Address_Pincode VARCHAR(10) NOT NULL,
+    Customer_Address_Pincode VARCHAR(10) NOT NULL DEFAULT 0,
     Customer_Contact_Number VARCHAR(15) NOT NULL,
-    Customer_Email VARCHAR(50) NOT NULL,
-    Customer_Cart_ID INT NOT NULL,
-    PRIMARY KEY (Customer_ID),
-    FOREIGN KEY (Customer_Cart_ID) REFERENCES cart(Cart_ID)
+    Customer_ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY
 );
 
-create table order_details(
-    Order_ID INT NOT NULL AUTO_INCREMENT, 
-    Order_Date_Time DATETIME NOT NULL,
-    Delivery_Date_Time DATETIME NOT NULL,
-    Order_Customer_ID INT NOT NULL,
-    PRIMARY KEY (Order_ID),
-    FOREIGN KEY (Order_Customer_ID) REFERENCES customer(Customer_ID)
+CREATE Table category(
+    Admin_ID INT NOT NULL,
+    Foreign Key (Admin_ID) REFERENCES admin_shop(Admin_ID),
+
+    Category_ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    Category_Name VARCHAR(50) NOT NULL
 );
+
+CREATE TABLE product(
+    Product_ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    product_name VARCHAR(50) NOT NULL,
+    product_price INT NOT NULL,
+    product_quantity INT NOT NULL,
+    Category_ID INT NOT NULL,
+    Admin_ID INT NOT NULL,
+    Foreign Key (Admin_ID) REFERENCES admin_shop(Admin_ID),
+
+    Foreign Key (Category_ID) REFERENCES category(Category_ID)
+    -- Category_ID INT NOT NULL FOREIGN KEY REFERENCES category(Category_ID)
+    -- Admin_ID INT NOT NULL FOREIGN KEY REFERENCES admin_shop(Admin_ID)        
+);
+
+CREATE TABLE orders(
+    Order_ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    Customer_ID INT NOT NULL,
+    Foreign Key (Customer_ID) REFERENCES customer(Customer_ID),
+    order_DateTime DATETIME NOT NULL,
+    order_amount INT NOT NULL,
+    Admin_ID INT NOT NULL,
+    Foreign Key (Admin_ID) REFERENCES admin_shop(Admin_ID)
+    -- Admin_ID INT NOT NULL FOREIGN KEY REFERENCES admin_shop(Admin_ID)
+);
+
+CREATE TABLE cart(
+    cart_ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    Customer_ID INT NOT NULL,
+    Foreign Key (Customer_ID) REFERENCES customer(Customer_ID),
+    Product_ID INT NOT NULL,
+    Foreign Key (Product_ID) REFERENCES product(Product_ID),
+    product_quantity INT NOT NULL,
+    product_price INT NOT NULL,
+    order_ID INT,
+    Admin_ID INT NOT NULL,
+    Foreign Key (Admin_ID) REFERENCES admin_shop(Admin_ID),
+    Foreign Key (order_ID) REFERENCES orders(order_ID)
+);
+
+
+CREATE Table delivery_man(
+    delivery_man_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    man_name VARCHAR(50) NOT NULL,
+    man_contact VARCHAR(15) NOT NULL,
+    man_email VARCHAR(50) NOT NULL,
+    man_pass VARCHAR(20) NOT NULL,
+    order_ID INT,
+    Foreign Key (order_ID) REFERENCES orders(order_ID),    -- Admin_ID INT NOT NULL FOREIGN KEY REFERENCES admin_shop(Admin_ID)
+    Admin_ID INT NOT NULL,
+    Foreign Key (Admin_ID) REFERENCES admin_shop(Admin_ID)
+);
+
+
+

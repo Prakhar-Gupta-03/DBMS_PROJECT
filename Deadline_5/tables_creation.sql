303ceeb38232dc75c4626761392ab38a2d31cd0f
Vartika2401 <97868917+Vartika2401@users.noreply.github.com>
2023-03-27 14:43:26 +0530

    Update tables_creation.sql

--- Deadline_5/tables_creation.sql
+++ Deadline_5/tables_creation.sql
@@ -1,5 +1,5 @@
-USE test;
-
+USE TEST;
+DROP TABLE IF EXISTS order_delivery_man;
 DROP TABLE IF EXISTS DELIVERY_MAN;
 DROP TABLE IF EXISTS CART;
 DROP TABLE IF EXISTS ORDERS;
@@ -8,10 +8,29 @@ DROP TABLE IF EXISTS CUSTOMER;
 DROP TABLE IF EXISTS CATEGORY;
 DROP TABLE IF EXISTS ADMIN_SHOP;
 
+
 CREATE Table admin_shop(
     Admin_ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    ADMIN_NAME VARCHAR(50) NOT NULL,
+    ADMIN_NAME VARCHAR(50) ,
     Admin_Password VARCHAR(50) NOT NULL
+);
+CREATE Table category(
+    Admin_ID INT NOT NULL,
+    Foreign Key (Admin_ID) REFERENCES admin_shop(Admin_ID),
+
+    Category_ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    Category_Name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE product(
+    Product_ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    product_name VARCHAR(50) NOT NULL,
+    product_price INT NOT NULL,
+    product_quantity INT NOT NULL,
+    Category_ID INT NOT NULL,
+    Admin_ID INT NOT NULL,
+    Foreign Key (Admin_ID) REFERENCES admin_shop(Admin_ID),
+    Foreign Key (Category_ID) REFERENCES category(Category_ID)       
 );
 
 CREATE Table customer(
@@ -29,33 +48,14 @@ CREATE Table customer(
     Customer_ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY
 );
 
-CREATE Table category(
-    Admin_ID INT,
-    Foreign Key (Admin_ID) REFERENCES admin_shop(Admin_ID),
-
-    Category_ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    Category_Name VARCHAR(50) NOT NULL
-);
-
-CREATE TABLE product(
-    Product_ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    product_name VARCHAR(50) NOT NULL,
-    product_price INT NOT NULL,
-    product_quantity INT NOT NULL,
-    Category_ID INT NOT NULL,
-    Admin_ID INT,
-    Foreign Key (Admin_ID) REFERENCES admin_shop(Admin_ID),
-    Foreign Key (Category_ID) REFERENCES category(Category_ID)       
-);
-
-
-CREATE TABLE orders(
-    Order_ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    Customer_ID INT NOT NULL,
-    Foreign Key (Customer_ID) REFERENCES customer(Customer_ID),
-    order_DateTime DATETIME NOT NULL,
-    order_amount INT NOT NULL DEFAULT 0,
-    Admin_ID INT,
+-- table only contains the personal details of the delivery man
+CREATE Table delivery_man(
+    delivery_man_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    man_name VARCHAR(50) NOT NULL,
+    man_contact VARCHAR(15) NOT NULL,
+    man_email VARCHAR(50) NOT NULL,
+    man_pass VARCHAR(20) NOT NULL,
+    Admin_ID INT NOT NULL,
     Foreign Key (Admin_ID) REFERENCES admin_shop(Admin_ID)
 );
 
@@ -69,26 +69,17 @@ CREATE TABLE cart(
     PRIMARY KEY (Customer_ID, Product_ID)
 );
 
--- table only contains the personal details of the delivery man
-CREATE Table delivery_man(
-    delivery_man_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    man_name VARCHAR(50) NOT NULL,
-    man_contact VARCHAR(15) NOT NULL,
-    man_email VARCHAR(50) NOT NULL,
-    man_pass VARCHAR(20) NOT NULL,
-    Admin_ID INT,
+CREATE TABLE orders(
+    Order_ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    Customer_ID INT NOT NULL,
+    Foreign Key (Customer_ID) REFERENCES customer(Customer_ID),
+    order_DateTime DATETIME NOT NULL,
+    order_amount INT NOT NULL DEFAULT 0,
+    Admin_ID INT NOT NULL,
     Foreign Key (Admin_ID) REFERENCES admin_shop(Admin_ID)
 );
 
--- maintains the relation between the delivery man and the order he is delivering
-CREATE TABLE order_delivery_man(
-    delivery_man_id INT NOT NULL, 
-    Foreign Key (delivery_man_id) REFERENCES delivery_man(delivery_man_id);
-    Order_ID INT NOT NULL,
-    Foreign Key (Order_ID) REFERENCES orders(Order_ID);
-    delivery_date DATETIME NOT NULL,
-    PRIMARY KEY (delivery_man_id, Order_ID)
-);
+
 
 -- maintains all the orders in the order history of all customers
 CREATE TABLE all_orders(
@@ -102,5 +93,16 @@ CREATE TABLE all_orders(
     Foreign Key (Customer_ID) REFERENCES customer(Customer_ID),
     PRIMARY KEY (Order_ID, Product_ID, Customer_ID)
 )
+
+ 
+-- maintains the relation between the delivery man and the order he is delivering
+CREATE TABLE order_delivery_man(
+    delivery_man_id INT NOT NULL, 
+    Foreign Key (delivery_man_id) REFERENCES delivery_man(delivery_man_id),
+    Order_ID INT NOT NULL,
+    Foreign Key (Order_ID) REFERENCES orders(Order_ID),
+    delivery_date DATETIME NOT NULL,
+    PRIMARY KEY (delivery_man_id, Order_ID)
+);
 
 
